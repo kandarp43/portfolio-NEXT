281a6050a0c899d4fe74972a0e7c7607d4782dcc
kandarp dangi <kandarpdangi@gmail.com>
2021-07-06 23:11:11 +0530

    init files and making navbar

---
+++ Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+function NavBar() {
+  return (
+    <div className='nav'>
+      <div className='nav-title'>kandarp</div>
+      <div className='nav-routes'>
+        <div className='sub-routes'>Home</div>
+        <div className='sub-routes'>Projects</div>
+        <div className='sub-routes'>about me</div>
+        <div className='sub-routes'>contact me </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../Components/NavBar/navbar.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
