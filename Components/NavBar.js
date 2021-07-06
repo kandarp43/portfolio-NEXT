aab37365562a638a84bfc62671366cb940af0a71
kandarp dangi <kandarpdangi@gmail.com>
2021-07-06 17:57:42 +0530

    init components

---
+++ Components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    
+  )
+}
+
+export default NavBar
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div>kd</div>
+  return (
+    <div>
+      <div className='main'></div>
+    </div>
+  )
 }
