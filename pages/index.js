f6cf73a81259a09272d0963ef2d527cf79d01670
kandarp dangi <kandarpdangi@gmail.com>
2021-07-08 23:37:29 +0530

    home page

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from 'next/image'
 import NavBar from '../Components/NavBar/NavBar'
 
 export default function Home() {
-  return <div className='home'></div>
+  return <div className='home'>Home</div>
 }
